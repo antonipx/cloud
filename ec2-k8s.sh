8e7c7e1b0291077a7fef1593e8d25f832eeefefb
Antoni Sawicki <as@portworx.com>
2018-07-30 13:42:49 -0700

    moved back

--- ec2-k8s.sh
+++ ec2-k8s.sh
@@ -6,13 +6,13 @@
 m="asm"
 s="as1 as2 as3"
 
-type="t2.micro" data_size="" ./ec2 create $m
+type="t2.medium" data_size="" ./ec2 create $m
 ./ec2 ssh $m < run/k8s-master.sh
 join=$(./ec2 ssh $m "cat /tmp/kubeadm-join.sh")
 
 for n in $s
 do 
-    type="t2.medium" ./ec2 create $n
+    type="r4.xlarge" ./ec2 create $n
     ./ec2 ssh $n < run/k8s-node.sh
     ./ec2 ssh $n "sudo $join"
 done
