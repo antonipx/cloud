82992bbb7fe133c8ae125a2bd35bcecb963a5af0
Antoni Sawicki <as@portworx.com>
2018-08-11 02:44:47 -0700

    remove px install from ubuntu run

--- run/ubuntu-run.sh
+++ run/ubuntu-run.sh
@@ -37,13 +37,5 @@ bind-key -n C-S-Left resize-pane -L 1
 bind-key -n C-S-Right resize-pane -R 1
 EOT
 
-
-# Portworx
-#docker run --rm --privileged=true --entrypoint /runc-entry-point.sh -v /opt/pwx:/opt/pwx -v /etc/pwx:/etc/pwx portworx/px-enterprise:1.3.4
-#/opt/pwx/bin/px-runc install -c $(cat /proc/sys/kernel/random/uuid | cut -f1 -d-) -k etcd://etcdv3-02.portworx.com:2379 -a -f
-#/opt/pwx/bin/px-runc install -c asnw3457 -k etcd://etcdv3-02.portworx.com:2379 -a -f
-#systemctl enable portworx
-#systemctl start portworx
-
 touch /var/lib/cloud/instance/user-init-finished
 
