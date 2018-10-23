b0d82b2268c41ead1f189244bc67c492042623ce
Antoni Sawicki <as@portworx.com>
2018-10-23 16:38:25 -0700

    renamed k8s scripts

--- ec2-k8s.sh
+++ ec2-k8s.sh
@@ -3,17 +3,17 @@
 # create 3 nodes kubernetes cluster + master on ec2
 #
 
-m="asm"
-s="as1 as2 as3"
+m="asxm"
+s="asx1 asx2 asx3"
 
 type="t2.medium" data_size="" ./ec2 create $m
-./ec2 ssh $m < run/k8s-master.sh
+./ec2 ssh $m < run/k8s-master
 join=$(./ec2 ssh $m "cat /tmp/kubeadm-join.sh")
 
 for n in $s
 do 
-    type="r4.xlarge" ./ec2 create $n
-    ./ec2 ssh $n < run/k8s-node.sh
+    type="t2.medium" ./ec2 create $n
+    ./ec2 ssh $n < run/k8s-node
     ./ec2 ssh $n "sudo $join"
 done
 
